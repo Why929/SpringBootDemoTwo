b79cd006c7ed1e1fc9ddecd098e2adcbafe0a8a5
Why929 <why4643@163.com>
2019-05-22 16:00:36 +0800

    SpringBoot_基本学习

--- src/main/java/cn/itcast/pojo/User.java
+++ src/main/java/cn/itcast/pojo/User.java
@@ -4,9 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity//依赖SpringJpa包//导入的是javax.persistence.Entity
-public class User {
+public class User implements Serializable{
 //1.映射主键:
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
